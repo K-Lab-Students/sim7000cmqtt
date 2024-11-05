51f4526fe222bfa3c74f07dbef093b0fa28f8b05
Uscera-Liberty <maslyayeva.yuliya@bk.ru>
2024-11-05 19:09:02 +0300

    считывание Vdd через Vrefint

--- src/ATCommunicator.cpp
+++ src/ATCommunicator.cpp
@@ -6,91 +6,39 @@
 
 #include <cstring>
 
-ATParser::Status ATParser::parse(const uint8_t *str, uint8_t size) noexcept
-{
-	uint8_t idx{};
-
-	while (str[idx] != '\r' && idx < size)
-		idx++;
-	if (str[idx + 1] == '\r')
-		idx++;
-
-	idx += 2;
-
-	if (idx >= size) {
-		return Status::kNotFullInput;
-	}
-
-	if (str[idx] == '>') {
-		return Status::kWaitInput;
-	}
-	if (str[idx] == '+') {
-		if (str[idx + 1] == 'C') {
-			if (str[idx + 2] == 'P') {
-				return Status::kCPIN;
-			} else if (str[idx + 2] == 'F') {
-				return Status::kCFUN;
-			} else {
-				return Status::kUnknown;
-			}
-		}
-		if (str[idx + 1] == 'A') {
-			if (str[idx + 10] == 'A') {
-				return Status::kAPPPDPActive;
-			} else if (str[idx + 10] == 'D') {
-				return Status::kAPPPDPDeactive;
-			} else {
-				return Status::kUnknown;
-			}
-		}
-		return Status::kUnknown;
-	} else if (str[idx] == 'S' && str[idx + 1] == 'M' && str[idx + 2] == 'S') {
-		return Status::kSMSRdy;
-	} else if (str[idx] == 'R' && str[idx + 1] == 'D' && str[idx + 2] == 'Y') {
-		return Status::kRDY;
-	} else if (str[idx] == 'O' && str[idx + 1] == 'K') {
-		return Status::kOk;
-	} else if (str[idx] == 'E' && str[idx + 1] == 'R' && str[idx + 2] == 'R') {
-		return Status::kError;
-	}
-
-	return Status::kNotFullInput;
-}
-
 ATCommunicator::ATCommunicator(UART_HandleTypeDef *huart) : huart_(huart)
 {
 }
 
-ATParser::Status ATCommunicator::rawSend(const std::string& str, uint32_t rx_attempts) noexcept
+ATCommunicator::Status ATCommunicator::rawSend(const char *str, char *resp, uint32_t timeout) noexcept
 {
-	HAL_UART_Transmit(huart_, reinterpret_cast<const uint8_t *>(str.c_str()), str.size(), 1000);
-	return waitResponse(rx_attempts);
+    auto hal_status = HAL_UART_Transmit(huart_, reinterpret_cast<const uint8_t *>(str), strlen(str), 1000);
+    if (hal_status == HAL_OK) {
+        return waitResponse(resp, timeout);
+    }
+    return hal_status == HAL_TIMEOUT ? Status::kTimeout : Status::kError;
 }
 
-ATParser::Status ATCommunicator::waitResponse(uint32_t rx_attempts) noexcept
+ATCommunicator::Status ATCommunicator::waitResponse(char *resp, uint32_t timeout) noexcept
 {
-	uint16_t size, idx{};
-	static uint8_t resp[256];
-	ATParser::Status res;
-	uint8_t attempts{};
+    uint16_t size, idx {};
 
-	do {
-		HAL_UARTEx_ReceiveToIdle(huart_, reinterpret_cast<uint8_t *>(rx_raw_buffer_),
-								 sizeof(rx_raw_buffer_), &size, 10000);
-		memcpy(&resp[idx], rx_raw_buffer_, size);
-		idx += size;
-		if (attempts > rx_attempts) {
-			res = ATParser::Status::kTimeoutError;
-		} else {
-			res = ATParser::parse(resp, idx);
-		}
-		attempts++;
-	}
-	while (res == ATParser::Status::kNotFullInput);
-	memset(rx_raw_buffer_, 0, sizeof(rx_raw_buffer_));
+    auto hal_status = HAL_UARTEx_ReceiveToIdle(huart_, rx_raw_buffer_, sizeof(rx_raw_buffer_), &size, timeout);
 
-	HAL_UART_Transmit(&huart3, resp, idx, 100);
-	memset(resp, 0, sizeof(resp));
+    if (resp != nullptr) {
+        memcpy(resp, rx_raw_buffer_, size);
+    }
 
-	return res;
+    memset(rx_raw_buffer_, 0, sizeof(rx_raw_buffer_));
+
+#ifdef SIM7000CMQTT_DEBUG_ENABLED
+    if (resp != nullptr) {
+        HAL_UART_Transmit(&huart3, resp, idx, 1000);
+    }
+#endif
+
+    if (hal_status == HAL_OK) {
+        return Status::kOk;
+    }
+    return hal_status == HAL_TIMEOUT ? Status::kTimeout : Status::kError;
 }
--- src/SIM7000MQTT.cpp
+++ src/SIM7000MQTT.cpp
@@ -5,124 +5,242 @@
 #include "SIM7000MQTT.hpp"
 #include "ATCommands.hpp"
 
-SIM7000MQTT::SIM7000MQTT(UART_HandleTypeDef *huart, URL url, Port port,
-						 CliendID client_id, Username username, Password password) :
-		comm_(huart),
-		url_(std::move(url)),
-		port_(std::move(port)),
-		client_id_(std::move(client_id)),
-		username_(std::move(username)),
-		password_(std::move(password))
+#include <cstring>
+
+#define RUN_OPERATION(OPERATION)           \
+    do {                                   \
+        if (OPERATION == Status::kError) { \
+            return Status::kError;         \
+        }                                  \
+    } while (0)
+
+SIM7000MQTT::SIM7000MQTT(UART_HandleTypeDef* huart, URL url, Port port, CliendID client_id, Username username,
+                         Password password) :
+    comm_(huart),
+    url_(std::move(url)),
+    port_(std::move(port)),
+    client_id_(std::move(client_id)),
+    username_(std::move(username)),
+    password_(std::move(password))
 {
-	setup_mqtt_cmds_ = std::vector<std::string>{
-			AT AT_ENDL,
-			AT_SMCONF_URL + url_ + "," + port_ + AT_ENDL,
-			AT_SMCONF_CLIENTID + client_id_ + AT_ENDL,
-			AT_SMCONF_USERNAME + username_ + AT_ENDL,
-			AT_SMCONF_PASSWORD + password_ + AT_ENDL,
-			AT_SMCONF_KEEPTIME_60 AT_ENDL
-	};
-
-	enable_mqtt_cmds_ = std::vector<std::string>{
-			AT_CNACT_ON AT_ENDL,
-			AT_SMCONN AT_ENDL
-	};
-
-	disable_mqtt_cmds_ = std::vector<std::string>{
-			AT_SMDISC AT_ENDL,
-			AT_CNACT_OFF AT_ENDL
-	};
 }
 
 void SIM7000MQTT::waitInit() noexcept
 {
-	uint8_t wait_sim_init_flags = 0;
-	do {
-		parser_status_ = comm_.waitResponse(1);
-		switch (parser_status_) {
-			case ATParser::Status::kCPIN:
-				wait_sim_init_flags |= 0b0001;
-				break;
-			case ATParser::Status::kRDY:
-				wait_sim_init_flags |= 0b1000;
-				break;
-			case ATParser::Status::kCFUN:
-				wait_sim_init_flags |= 0b0010;
-				break;
-			case ATParser::Status::kSMSRdy:
-				wait_sim_init_flags |= 0b0100;
-				break;
-			default:
-				break;
-		}
-	}
-	while (wait_sim_init_flags < 0b0111);
+    uint8_t success_cnt = 0;
+    uint8_t timeout = HAL_GetTick();
+    char resp[256] {};
+    do {
+        if (HAL_GetTick() - timeout >= 15000) {
+            break;
+        }
+
+        auto comm_status = comm_.waitResponse(resp, 500);
+
+        if (comm_status == ATCommunicator::Status::kOk) {
+            success_cnt++;
+        }
+
+    } while (success_cnt < 3);
+
+    comm_.rawSend("ATE0\r", resp, 500);
 }
 
-void SIM7000MQTT::setupMQTT() noexcept
+SIM7000MQTT::Status SIM7000MQTT::setupMQTT() noexcept
 {
-	for (uint8_t i = 0; i < setup_mqtt_cmds_.size();) {
-		HAL_Delay(300);
-		auto s = comm_.rawSend(setup_mqtt_cmds_[i], 1);
-		if (s == ATParser::Status::kOk)
-			++i;
-	}
+    RUN_OPERATION(ATsmconf_("URL", url_.c_str(), port_));
+    RUN_OPERATION(ATsmconf_("CLIENTID", client_id_.c_str()));
+    RUN_OPERATION(ATsmconf_("USERNAME", username_.c_str()));
+    RUN_OPERATION(ATsmconf_("PASSWORD", password_.c_str()));
+    RUN_OPERATION(ATsmconf_("KEEPTIME", "60"));
+    return Status::kOk;
 }
 
-void SIM7000MQTT::enableMQTT() noexcept
+SIM7000MQTT::Status SIM7000MQTT::enableWirelessConnection() noexcept
 {
-
-	for (uint8_t i = 0; i < enable_mqtt_cmds_.size();) {
-		HAL_Delay(300);
-		auto s = comm_.rawSend(enable_mqtt_cmds_[i], 5);
-		if (s == ATParser::Status::kOk) {
-			if (i == 0) {
-				if (comm_.waitResponse(5) == ATParser::Status::kAPPPDPActive) {
-					++i;
-				}
-			} else if (i == 1) {
-				++i;
-			}
-		}
-	}
+    RUN_OPERATION(ATcnact_(true));
+    return Status::kOk;
 }
 
-void SIM7000MQTT::disableMQTT() noexcept
+SIM7000MQTT::Status SIM7000MQTT::disableWirelessConnection() noexcept
 {
-	for (uint8_t i = 0; i < disable_mqtt_cmds_.size();) {
-		HAL_Delay(300);
-		auto s = comm_.rawSend(disable_mqtt_cmds_[i], 5);
-		if (s == ATParser::Status::kOk) {
-			if (i == 0) {
-				++i;
-			} else if (i == 1) {
-				if (comm_.waitResponse(5) == ATParser::Status::kAPPPDPDeactive) {
-					++i;
-				}
-			}
-		}
-	}
+    RUN_OPERATION(ATcnact_(false));
+    return Status::kOk;
 }
 
-void SIM7000MQTT::setupGNSS(const SIM7000MQTT::Topic& topic, uint32_t timeout) noexcept
+SIM7000MQTT::Status SIM7000MQTT::enableMQTT() noexcept
 {
+    RUN_OPERATION(ATsmconn_(true));
+    return Status::kOk;
 }
 
-void SIM7000MQTT::publishMessage(const SIM7000MQTT::Topic& topic, const std::string& message) noexcept
+SIM7000MQTT::Status SIM7000MQTT::disableMQTT() noexcept
 {
-	publish_message_cmds_[0] = AT_SMPUB"\"" + topic + "\"," + std::to_string(message.size()) + ",1,1" + AT_ENDL;
-	publish_message_cmds_[1] = message + AT_ENDL;
-	for (uint8_t i = 0; i < publish_message_cmds_.size();) {
-		HAL_Delay(300);
-		auto s = comm_.rawSend(publish_message_cmds_[i], 5);
-		if (i == 0) {
-			if (s == ATParser::Status::kWaitInput) {
-				++i;
-			}
-		} else if (i == 1) {
-			if (s == ATParser::Status::kOk) {
-				++i;
-			}
-		}
-	}
+    RUN_OPERATION(ATsmconn_(false));
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::enableGNSS() noexcept
+{
+    RUN_OPERATION(ATgnspwr_(true));
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::disableGNSS() noexcept
+{
+    RUN_OPERATION(ATgnspwr_(false));
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::getGNSS(SIM7000MQTT::GPS& data) noexcept
+{
+    char nmeabuff[256];
+    RUN_OPERATION(ATgnsinf_(nmeabuff));
+
+    // parse +CGNSINF: 1,1,20240704144820.000,45.020006,39.030315,94.600,0.00,153.0,1,,0.8,1.1,0.8,,8,7,,,47,,
+
+    // skip GPS run status
+    char* tok = strtok(nmeabuff, ",");
+    if (!tok) {
+        return Status::kError;
+    }
+
+    // skip fix status
+    tok = strtok(NULL, ",");
+    if (!tok) {
+        return Status::kError;
+    }
+
+    // skip date
+    tok = strtok(NULL, ",");
+    if (!tok) {
+        return Status::kError;
+    }
+
+    // grab the latitude
+    char* latp = strtok(NULL, ",");
+    if (!latp) {
+        return Status::kError;
+    }
+    // grab longitude
+    char* longp = strtok(NULL, ",");
+    if (!longp) {
+        return Status::kError;
+    }
+
+    data.latitude = atof(latp);
+    data.longitude = atof(longp);
+
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::publishMessage(const SIM7000MQTT::Topic& topic, const std::string& message) noexcept
+{
+    RUN_OPERATION(ATsmpub_(topic.c_str(), message.c_str()));
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::ATsmconf_(const char* parameter, const char* value) noexcept
+{
+    char msg[100] {};
+    sprintf(msg, "AT+SMCONF=\"%s\",\"%s\"\r", parameter, value);
+
+    auto comm_status = comm_.rawSend(msg, resp_, 500);
+
+    if (comm_status != ATCommunicator::Status::kOk || checkResp_(resp_, "OK") != Status::kOk)
+        return Status::kError;
+
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::ATsmconf_(const char* parameter, const char* value, uint16_t port) noexcept
+{
+    char msg[100] {};
+    sprintf(msg, "AT+SMCONF=\"%s\",\"%s\",%i\r", parameter, value, port);
+
+    auto comm_status = comm_.rawSend(msg, resp_, 500);
+
+    if (comm_status != ATCommunicator::Status::kOk || checkResp_(resp_, "OK") != Status::kOk)
+        return Status::kError;
+
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::ATcnact_(bool status) noexcept
+{
+    char msg[100] {};
+    sprintf(msg, "AT+CNACT=%d\r", status ? 1 : 0);
+
+    while (true) {
+        auto comm_status = comm_.rawSend(msg, resp_, 1000);
+        if (comm_status != ATCommunicator::Status::kOk || checkResp_(resp_, "OK") != Status::kOk)
+            return Status::kError;
+
+        comm_status = comm_.waitResponse(resp_, 32000);
+        if (comm_status == ATCommunicator::Status::kTimeout || comm_status == ATCommunicator::Status::kError)
+            return Status::kError;
+
+        if (checkResp_(resp_, status ? "ACTIVE" : "DEACTIVE") == Status::kOk)
+            break;
+    }
+
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::ATsmconn_(bool status) noexcept
+{
+    auto comm_status = comm_.rawSend(status ? "AT+SMCONN\r" : "AT+SMDISC\r", resp_, 5000);
+
+    if (comm_status != ATCommunicator::Status::kOk || checkResp_(resp_, "OK") != Status::kOk)
+        return Status::kError;
+
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::ATgnspwr_(bool status) noexcept
+{
+    char msg[100] {};
+    sprintf(msg, "AT+CGNSPWR=%d\r", status ? 1 : 0);
+
+    auto comm_status = comm_.rawSend(msg, resp_, 5000);
+
+    if (comm_status != ATCommunicator::Status::kOk || checkResp_(resp_, "OK") != Status::kOk)
+        return Status::kError;
+
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::ATsmpub_(const char* topic, const char* message) noexcept
+{
+    char msg[100] {};
+    sprintf(msg, "AT+SMPUB=\"%s\",%d,1,1\r", topic, strlen(message));
+
+    auto comm_status = comm_.rawSend(msg, resp_, 2000);
+
+    if (comm_status != ATCommunicator::Status::kOk || checkResp_(resp_, ">") != Status::kOk)
+        return Status::kError;
+
+    sprintf(msg, "%s\r", message);
+    comm_status = comm_.rawSend(message, resp_, 5000);
+    if (comm_status != ATCommunicator::Status::kOk || checkResp_(resp_, "OK") != Status::kOk)
+        return Status::kError;
+
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::ATgnsinf_(char* reply) noexcept
+{
+    auto comm_status = comm_.rawSend("AT+CGNSINF\r", reply, 5000);
+
+    if (comm_status != ATCommunicator::Status::kOk)
+        return Status::kError;
+
+    comm_status = comm_.waitResponse(nullptr, 5000);
+
+    return Status::kOk;
+}
+
+SIM7000MQTT::Status SIM7000MQTT::checkResp_(const char* resp, const char* except) noexcept
+{
+    return strstr(resp, except) == 0 ? Status::kError : Status::kOk;
 }
